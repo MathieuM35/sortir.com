53c51f87fd14352847bf8190acdb15c265f6000a
lucie.rabiniaux <lucie.rabiniaux@gmail.com>
2019-05-20 16:52:31 +0200

    Création du form "creer_une_sortie"
    Formulaire de création d'une sortie (Controller+Twig associé)

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends Controller
@@ -17,7 +22,32 @@ class SortieController extends Controller
         ]);
     }
 
-    public function add(){
+    /**
+     * Page Créer une sortie
+     * @Route("/creer-sortie",name="creer_sortie")
+     * @Template()
+     */
+    public function add(Request $request,
+                        EntityManagerInterface $em)
+    {
+        $sortie = new Sortie();
+        $sortie->setEtat('Créée');
+        $sortie->setOrganisateur($this->getUser());
+        $sortie->setParticipants(array());
+
+        $formSortie = $this->createForm(SortieType::class, $sortie);
+        $formSortie->handleRequest($request);
+
+        if ($formSortie->isSubmitted() && $formSortie->isValid()) {
+            $em->persist($sortie);
+            $em->flush();
+
+            $this->addFlash("success", "Votre sortie a bien été ajoutée !");
+            return $this->redirectToRoute("details", ['id' => $sortie->getId()]);
+        } else {
+            return (['formSortie'=>$formSortie->createView()]);
+        }
+
 
     }
 }
--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,8 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +19,17 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie')
-            ->add('etat')
+            ->add('nom',TextType::class)
+            ->add('dateHeureDebut',DateType::class,['widget'=>'single_text'])
+            ->add('dateLimiteInscription',DateType::class,['widget'=>'single_text'])
+            ->add('nbInscriptionsMax',IntegerType::class)
+            ->add('duree',IntegerType::class)
+            ->add('infosSortie',TextareaType::class)
+
+            ->add('lieu',EntityType::class,[
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+            ])
         ;
     }
 
